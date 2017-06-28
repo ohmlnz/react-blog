57aa4f0d82cc5bae0129c9f2ab8083871d367027
OthmanLan <othman.lanizi@gmail.com>
2017-06-28 02:01:07 -0400

    Add timestamp

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
+import Moment from 'react-moment';
 
 import { Pagination } from '../components/Pagination' 
 import '../css/ArticlesList.css'
@@ -10,6 +11,8 @@ export const ArticlesList = (props) => (
 		<Panel header={a.title} key={a.id}>
 			<i className="fa fa-trash-o" aria-hidden="true" style={{display: props.removeState}} onClick={props.removeArticle} id={a.id}></i>
 			<span dangerouslySetInnerHTML={{__html: a.content}} />
+			<hr/>
+			<span className='timestamp'>Posted <Moment fromNow>{a.timestamp}</Moment></span>
 		</Panel>)}
 		<Pagination/>
 	</div>
