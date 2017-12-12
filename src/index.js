45cf92e165d8cd1978d1ab292adec44876e6212d
OthmanLan <othman.lanizi@gmail.com>
2017-12-12 18:10:07 -0500

    Stupid Redux extension.

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ let store = createStore(
 	defaultState,
 	compose(
 		middleware,
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	) 
 );
 
